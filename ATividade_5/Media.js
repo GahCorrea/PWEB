bfdb684b608cdfc081929627fc81696c9942cd89
Gabriel Corrêa <50000690+GahCorrea@users.noreply.github.com>
2020-09-28 12:21:59 -0300

    Delete Media.js

--- ATividade_5/Media.js
+++
@@ -1,7 +0,0 @@
-var nome = prompt("Digite seu nome:");
-
-var n1 = prompt("Digite a primeira nota:");
-var n2 = prompt("Digite a segunda nota:");
-var n3 = prompt("Digite a terceira nota");
-
-alert(nome + " sua nota é " + ((parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3).toFixed(2));
