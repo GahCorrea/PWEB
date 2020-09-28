644734fcd04397ac00a111a9fdb683bb73a5e414
Gabriel Corrêa <50000690+GahCorrea@users.noreply.github.com>
2020-09-28 12:11:53 -0300

    Add files via upload

--- ATividade_5/Media.js
+++ ATividade_5/Media.js
@@ -4,6 +4,4 @@ var n1 = prompt("Digite a primeira nota:");
 var n2 = prompt("Digite a segunda nota:");
 var n3 = prompt("Digite a terceira nota");
 
-var media = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3;
-
-alert(media);
+alert(nome + " sua nota é " + (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3);
