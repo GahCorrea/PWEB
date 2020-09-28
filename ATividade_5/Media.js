6c01893df5018891e8448f954a68342240321547
Gabriel Corrêa <50000690+GahCorrea@users.noreply.github.com>
2020-09-28 12:07:45 -0300

    Add files via upload

---
+++ ATividade_5/Media.js
@@ -0,0 +1,9 @@
+var nome = prompt("Digite seu nome:");
+
+var n1 = prompt("Digite a primeira nota:");
+var n2 = prompt("Digite a segunda nota:");
+var n3 = prompt("Digite a terceira nota");
+
+var media = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3;
+
+alert(media);
