2a85f12ffc2e1a32c1934aef3e0a2f2d0ba84600
Gabriel Corrêa <50000690+GahCorrea@users.noreply.github.com>
2020-09-28 12:20:42 -0300

    Add files via upload

---
+++ Atividade_5/Media.js
@@ -0,0 +1,7 @@
+var nome = prompt("Digite seu nome:");
+
+var n1 = prompt("Digite a primeira nota:");
+var n2 = prompt("Digite a segunda nota:");
+var n3 = prompt("Digite a terceira nota");
+
+alert(nome + " sua nota é " + ((parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3).toFixed(2));
---
+++ Atividade_5/Operacoes.js
@@ -0,0 +1,10 @@
+var n1 = prompt("Digite o primeiro número:");
+var n2 = prompt("Digite o segundo número:");
+
+alert(
+    "Soma = " + (parseFloat(n1) + parseFloat(n2)) +
+    "\nSubtração = " + (parseFloat(n1) - parseFloat(n2)) +
+    "\nProduto = " + (parseFloat(n1) * parseFloat(n2)) +
+    "\nDivisão = " + (parseFloat(n1) / parseFloat(n2)) +
+    "\nResto de divisão = " + (parseFloat(n1) % parseFloat(n2))
+);
