01861552bdd7092e0d0e25c4b6bbd997c86ae608
Gabriel Corrêa <50000690+GahCorrea@users.noreply.github.com>
2020-10-05 20:30:21 -0300

    Add files via upload

--- Atividade_6/Jokenpo.js
+++ Atividade_6/Jokenpo.js
@@ -1,4 +1,4 @@
-function pc() {
+function random() {
     return parseInt(Math.random() * (4 - 1) + 1);
 }
 
@@ -12,15 +12,16 @@ function play() {
         escolha = 3;
     }
 
-    console.log(pc(), escolha);
+    var pc = random();
+    console.log(pc, escolha);
 
-    if (pc() === escolha) {
+    if (pc === escolha) {
         document.getElementById("Ganhador").value = "Empate";
-    } else if ((pc() == 1 && escolha == 3) || (pc() == 3 && escolha == 1)) {
+    } else if ((pc == 1 && escolha == 3) || (pc == 3 && escolha == 1)) {
         document.getElementById("Ganhador").value = "Pedra quebra tesoura";
-    } else if ((pc() == 2 && escolha == 1) || (pc() == 1 && escolha == 2)) {
+    } else if ((pc == 2 && escolha == 1) || (pc == 1 && escolha == 2)) {
         document.getElementById("Ganhador").value = "Papel cobre pedra";
-    } else if ((pc() == 3 && escolha == 2) || (pc() == 2 && escolha == 3)) {
+    } else if ((pc == 3 && escolha == 2) || (pc == 2 && escolha == 3)) {
         document.getElementById("Ganhador").value = "Tesoura corta papel";
     }
 }
