a5e5e3d47254de551ba0209a93fd82a0c3ef5eff
Gabriel Corrêa <50000690+GahCorrea@users.noreply.github.com>
2020-10-19 12:40:25 -0300

    Add files via upload

---
+++ Atividade_8/aluno.js
@@ -0,0 +1,21 @@
+//Objeto 1
+var alunoObj1 = new Object();
+alunoObj1.ra1 = '0030481821042';
+alunoObj1.nome1 = 'Gabriel Corrêa';
+
+alert("RA: " + alunoObj1.ra1 + "\nNome: " + alunoObj1.nome1);
+
+//Objeto 2
+var alunoObj2 = {};
+alunoObj2.ra2 = '0030481821002';
+alunoObj2.nome2 = 'Lucas da Silva Barros';
+
+alert("RA: " + alunoObj2.ra2 + "\nNome: " + alunoObj2.nome2);
+
+//Objeto 3
+var alunoObj3 = {
+    ra3: '0030481821009',
+    nome3: 'Rafael Luiz Pereira'
+};
+
+alert("RA: " + alunoObj3.ra3 + "\nNome: " + alunoObj3.nome3);
